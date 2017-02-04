700671e7a67081e14b690d91445e5c44cd6db7f9
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-04 17:37:41 +0200

    wrote code test for the first exercise in oop folder

--- oop/calc_1.rb
+++ oop/calc_1.rb
@@ -42,3 +42,72 @@ end
 
 # Write your own driver code below:
 
+calculator = FancyCalculator.new
+addition = calculator.add(10, 2)
+subtraction = calculator.subtract(10, 2)
+multiplication = calculator.multiply(10, 2)
+division = calculator.divide(10, 2)
+inverse = calculator.square_root(4)
+
+
+puts "TESTING SimpleCalculator..."
+puts
+
+result = addition
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 12
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = subtraction
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 8
+  puts "PASS!"
+else
+  puts "F"
+end
+result = multiplication
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 20
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = division
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 5
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = inverse
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 2
+  puts "PASS!"
+else
+  puts "F"
+end
