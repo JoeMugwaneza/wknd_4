9c35bd5be662f2349a4d88e323384d01ce3b39b0
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-04 18:10:10 +0200

    wrote code test for third exercise in oop folder

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -30,14 +30,12 @@ class SimpleCalculator
 end
 
 class FancyCalculator
-  include Common_functions
-  end
 
+  include Common_functions
   def square_root(number)
     Math.sqrt(number)
   end
-
-end
+end 
 
 class WhizBangCalculator < FancyCalculator
 
@@ -55,3 +53,97 @@ end
 
 # Copy your driver code from the previous exercise and more below:
 
+supper_calculator = WhizBangCalculator.new
+addition = supper_calculator.add(4, 3)
+subtraction = supper_calculator.subtract(4, 3)
+multiplication = supper_calculator.multiply(4, 3)
+division = supper_calculator.divide(6, 3)
+inverse = supper_calculator.square_root(4)
+hypot = supper_calculator.hypotenuse(4, 3)
+power = supper_calculator.exponent(4, 2)
+
+puts "TESTING SimpleCalculator..."
+puts
+
+result = addition
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 7
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = subtraction
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 1
+  puts "PASS!"
+else
+  puts "F"
+end
+result = multiplication
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 12
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = division
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 2
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = hypot
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 5
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = inverse
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 2
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = power
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 16
+  puts "PASS!"
+else
+  puts "F"
+end
